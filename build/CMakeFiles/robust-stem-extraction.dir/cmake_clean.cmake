e512c7f5a1b4170ca0f44663047dce4baf518caf
ShengDao Du <r08921101@ntu.edu.tw>
2023-10-02 12:41:42 +0800

    create init worksapece

---
+++ build/CMakeFiles/robust-stem-extraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robust-stem-extraction.dir/src/robust-stem-extraction.cpp.o"
+  "robust-stem-extraction"
+  "robust-stem-extraction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robust-stem-extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
